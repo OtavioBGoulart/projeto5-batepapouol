6c773b213a7f5378a30d01f9b78be1b83fcc0486
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-08-29 01:05:49 -0300

    versao final sem bonus

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let mensagens = [];
-let meuNome = "Otavio";
+let cadastroNome;
+let nome;
 
 function atualizarMensagens() {
     setInterval(receba, 3000);
@@ -25,19 +26,25 @@ function renderizarMensagens(dados) {
 
             containerMensagens.innerHTML = containerMensagens.innerHTML + `
         <li class = "saiu-entrou">
-        <span class="cor-horario">(${mensagens[i].time}) </span> <span class="negrito-nome"> ${mensagens[i].from} </span>  ${mensagens[i].text}
+        <span class="cor-horario">(${mensagens[i].time})</span> <span class="negrito-nome"> <strong>${mensagens[i].from}</strong> </span>  ${mensagens[i].text}
         </li> `;
 
-        } else if (mensagens[i].to == meuNome) {
+        } else if (mensagens[i].to == cadastroNome && mensagens[i].type == "private_message") {
             containerMensagens.innerHTML = containerMensagens.innerHTML + `
         <li class="reservado">
-        (${mensagens[i].time}) ${mensagens[i].from} reservadamente para ${meuNome}: ${mensagens[i].text}}
+        <span class="cor-horario">(${mensagens[i].time})</span> <span class="negrito-nome"> <strong>${mensagens[i].from}</strong> </span> reservadamente para <span class="negrito-nome"> <strong>${cadastroNome}:</strong> </span> ${mensagens[i].text}
         </li>  `;
 
-        } else {
+        } else if (mensagens[i].to !== "Todos" && mensagens[i].type == "message") {
             containerMensagens.innerHTML = containerMensagens.innerHTML + `
         <li>
-        (${mensagens[i].time}) ${mensagens[i].from} para todos: ${mensagens[i].text}
+        <span class="cor-horario">(${mensagens[i].time})</span> <span class="negrito-nome"> <strong>${mensagens[i].from}</strong> </span> para <span class="negrito-nome"> <strong>${mensagens[i].to}:</strong> </span> ${mensagens[i].text}
+        </li>  `
+
+        } else if (mensagens[i].to == "Todos") {
+            containerMensagens.innerHTML = containerMensagens.innerHTML + `
+        <li>
+        <span class="cor-horario">(${mensagens[i].time})</span> <span class="negrito-nome"> <strong>${mensagens[i].from}</strong> </span> para <span class="negrito-nome"> <strong>todos</strong> </span>: ${mensagens[i].text}
         </li>  `
 
         }
@@ -47,12 +54,14 @@ function renderizarMensagens(dados) {
 
 atualizarMensagens();
 cadastro();
+manterConexaoSempre();
+
 
 function cadastro() {
 
-    let cadastroNome = prompt("Seja bem Vindo, qual seu nome?");
+    cadastroNome = prompt("Seja bem Vindo, qual seu nome?");
 
-    const nome = {
+    nome = {
         name: cadastroNome
     };
 
@@ -64,3 +73,37 @@ function nomeJaExiste() {
     alert("Esse nome já existe, digite outro");
     cadastro();
 }
+
+function manterConexao() {
+
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nome);
+}
+
+function manterConexaoSempre() {
+    setInterval(manterConexao, 5000);
+}
+
+function enviarMensagem() {
+
+    let selecionarMensagem = document.querySelector("input");
+    let mensagemSelecionada = selecionarMensagem.value;
+    console.log(mensagemSelecionada);
+    let mensagem = {
+        from: cadastroNome,
+        to: "Todos",
+        text: mensagemSelecionada,
+        type: "message"
+    };
+
+    const envioMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
+    envioMensagem.catch(errorMensagem);
+
+    selecionarMensagem.value = "";
+}
+
+function errorMensagem() {
+    window.location.reload();
+}
+
+
+
